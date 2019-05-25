6850c198bc209c5272ec5604259cdd8e5ec5deee
Danielle Nourok <dnourok1@gmail.com>
2019-05-25 10:11:07 -0400

    remove comment from old example and fix spacing

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import LoginForm from './components/LoginForm'
 import PatientForm from './components/PatientForm'
 import DoctorView from './components/PatientList'
 
-// The Main component renders one of the three provided
-// Routes (provided that one matches). Both the /roster
-// and /schedule routes will match any pathname that starts
-// with /roster or /schedule. The / route will only match
-// when the pathname is exactly the string "/"
 const App = () => (
   <main>
     <Switch>
