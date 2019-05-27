26b5132e625a3be3bb7fd3f220c2de6e13f9f9fd
Danielle Nourok <dnourok1@gmail.com>
2019-05-27 11:16:23 -0400

    set up mongodb

--- server/index.js
+++ server/index.js
@@ -1,10 +1,33 @@
-const express = require("express");
+// import express from 'express';
+const MongoClient = require('mongodb').MongoClient;
+// const PORT = process.env.PORT || 3000 
+// const server = express();
+const uri =  'mongodb+srv://dnourok:' + password + '@portal-lsi4f.mongodb.net/'
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
-const PORT = process.env.PORT || 3000 
-const server = express();
+client.connect(function(err,db){
+  if(err){
+      console.error(err);
+      console.error('db', db)
+  }
+  else {
+      console.log('connected to '+ uri);
+      client.db("sample_weatherdata").collection('data').findOne({},function(err, data) {
+        console.log(data)
+    });
+  }
+})
 
-server.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`);
-});
+
+// server.listen(PORT, () => {
+//   console.log(`Listening on http://localhost:${PORT}`);
+// });
+
+
+
+
+
+
+
 
 
