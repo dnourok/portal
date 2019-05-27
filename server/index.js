2195145fe644e4ac95a2ad7897773904e1044d38
Danielle Nourok <dnourok1@gmail.com>
2019-05-27 11:26:11 -0400

    save data to new file

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 // import express from 'express';
 const MongoClient = require('mongodb').MongoClient;
+const fs = require('fs');
 // const PORT = process.env.PORT || 3000 
 // const server = express();
 const uri =  'mongodb+srv://dnourok:' + password + '@portal-lsi4f.mongodb.net/'
@@ -14,9 +15,18 @@ client.connect(function(err,db){
       console.log('connected to '+ uri);
       client.db("sample_weatherdata").collection('data').findOne({},function(err, data) {
         console.log(data)
+        
+        // write data to a new file named patientData.js
+        fs.writeFile('patientData.js', data, (err) => {  
+          if (err) throw err;
+          console.log('data saved!');
+        });
     });
+    client.close();
   }
 })
+
+
 
 
 // server.listen(PORT, () => {
